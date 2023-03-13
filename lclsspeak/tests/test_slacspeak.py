5a383c74baeb96e666f8587d0571efa86377cfc9
Ken Lauer <klauer@slac.stanford.edu>
2023-03-13 16:22:37 -0700

    FIX: manually fix slacspeak html

--- lclsspeak/tests/test_slacspeak.py
+++ lclsspeak/tests/test_slacspeak.py
@@ -4,8 +4,20 @@ from .. import slacspeak
 def test_parse():
     definitions = slacspeak.get_packaged_slacspeak()
     assert len(definitions)
-    
+
     name_to_defn = {defn.name: defn for defn in definitions}
-    zfs = name_to_defn['ZFS']
-    assert zfs.definition == 'Hypothetical, highly exotic source capable of accelerating particles to 1 ZeV.'
-    assert "slacspeak" in zfs.tags
+    zevatron = name_to_defn['Zevatron']
+    assert zevatron.definition == 'Hypothetical, highly exotic source capable of accelerating particles to 1 ZeV.'
+    assert "slacspeak" in zevatron.tags
+
+    bsl = name_to_defn['BSL']
+    assert bsl.definition == 'BioSafety Level'
+
+    last = None
+    for name, defn in name_to_defn.items():
+        if name not in ("SPEAR", ):
+            assert len(defn.definition) < 1000, f"{name} or {last} may have malformed html"
+        last = name
+
+    bsl1 = name_to_defn['BSL-1']
+    assert bsl1.definition == 'BioSafety Level 1, a basic level of containment defined by the CDC that relies on standard microbiological practices.'
